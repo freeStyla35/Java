df2e91556686fdb6e45787518483836469634c76
cihan <cihan@cihans-mbp.attlocal.net>
2018-11-26 21:42:32 -0600

    JavaProject03

---
+++ 03-Operators/src/RelationOperators.java
@@ -0,0 +1,14 @@
+
+public class RelationOperators {
+
+	public static void main(String[] args) {
+		int i1=5;
+		int i2=10;
+		
+		System.out.println(i1==i2);
+		System.out.println(i1!=i2);
+		System.out.println(i1>=i2);
+		System.out.println(i1<=i2);	
+
+	}
+}
---
+++ 03-Operators/src/Task.java
@@ -0,0 +1,14 @@
+
+public class Task {
+
+	public static void main(String[] args) {
+		double d1  =20.0;
+		double d2  =80.0;
+		double res = (d1+d2)*25;
+		double res2 = res % 40;
+		System.out.println(res2);
+		System.out.println(res2 <= 20);
+
+	}
+
+}
